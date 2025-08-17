6f2ef41a8531e63c84acc7d50be29f844fa2cc29
Gustavo Amezcua <gusta2897@gmail.com>
2025-08-16 20:44:10 -0700

    left at Home Property Listings

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -48,12 +48,21 @@ const PropertyCard = ({ property }) => {
         </div>
 
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
-          <p>
-            <FaMoneyBill className="md:hidden lg:inline mr-2" /> Weekly
-          </p>
-          <p>
-            <FaMoneyBill className="md:hidden lg:inline mr-2" /> Monthly
-          </p>
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className="md:hidden lg:inline mr-2" /> Nightly
+            </p>
+          )}
+          {property.rates.weekly && (
+            <p>
+              <FaMoneyBill className="md:hidden lg:inline mr-2" /> Weekly
+            </p>
+          )}
+          {property.rates.monthly && (
+            <p>
+              <FaMoneyBill className="md:hidden lg:inline mr-2" /> Monthly
+            </p>
+          )}
         </div>
 
         <div className="border border-gray-100 mb-5"></div>

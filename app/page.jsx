67929e08c78c30cc489c472a82b57cf009640f91
Gustavo Amezcua <gusta2897@gmail.com>
2025-08-18 22:39:59 -0700

    left at creating Properties Schema at Property & User Models

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,7 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
-
-export const metadata = {
-  title: "Home Page",
-};
+import connectDB from "@/config/database";
 
 const HomePage = () => {
   return (

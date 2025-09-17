9d34782d76be68a4e7597caaafae0b0ccdd960de
Gustavo Amezcua <gusta2897@gmail.com>
2025-09-17 16:15:12 -0700

    Left at Single Property Page

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch All Properties
 async function fetchProperties() {
   try {
     if (!apiDomain) {
@@ -19,4 +20,24 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch Single Property
+async function fetchProperty(id) {
+  try {
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };

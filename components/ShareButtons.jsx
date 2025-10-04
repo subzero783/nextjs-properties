40b1d16c04fb232ce309801e64f863a03bec36ff
Gustavo Amezcua <gusta2897@gmail.com>
2025-10-03 19:43:21 -0700

    Left at Share Buttons

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,6 +1,8 @@
-import { FaShare } from "react-icons/fa";
+import { FacebookMessengerShareButton, TwitterShareButton, WhatsappShareButton, EmailShareButton, FacebookIcon, TwitterIcon, EmailIcon } from "react-share";
 
-const ShareButtons = () => {
+const ShareButtons = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+
   return (
     <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
       <FaShare className="mr-2" /> Share Property

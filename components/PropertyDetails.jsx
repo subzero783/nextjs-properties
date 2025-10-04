9f5083c1290adb6490e6cd62a66b85738eddf5a7
Gustavo Amezcua <gusta2897@gmail.com>
2025-10-03 17:07:50 -0700

    Left at Bookmark API Route

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,4 +1,5 @@
 import { FaBed, FaBath, FaRulerCombined, FaTimes, FaCheck, FaMapMarker } from "react-icons/fa";
+import PropertyMap from "./PropertyMap";
 
 const PropertyDetails = ({ property }) => {
   return (
@@ -56,7 +57,7 @@ const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map" />
+        <PropertyMap property={property} />
       </div>
     </main>
   );

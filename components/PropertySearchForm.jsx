86742ab6cdd80bc118731dd1ccf8ba61bff1f19e
Gustavo Amezcua <gusta2897@gmail.com>
2025-10-05 12:04:29 -0700

    Left at Message Model & Form State

--- components/PropertySearchForm.jsx
+++ components/PropertySearchForm.jsx
@@ -1,12 +1,28 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const PropertySearchForm = () => {
   const [location, setLocation] = useState("");
   const [propertyType, setPropertyType] = useState("All");
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (location === "" && propertyType === "All") {
+      router.push("/properties/");
+    } else {
+      const query = `?location=${location}&propertyType=${propertyType}`;
+
+      router.push(`/properties/search-results${query}`);
+    }
+  };
 
   return (
-    <form className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center">
+    <form
+      onSubmit={handleSubmit}
+      className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
+    >
       <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
         <label
           htmlFor="location"
@@ -33,6 +49,8 @@ const PropertySearchForm = () => {
         <select
           id="property-type"
           className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+          value={propertyType}
+          onChange={(e) => setPropertyType(e.target.value)}
         >
           <option value="All">All</option>
           <option value="Apartment">Apartment</option>

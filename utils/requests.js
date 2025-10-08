459e15d1c1455f5db14b73a91470ee3bf64bc9b2
Gustavo Amezcua <gusta2897@gmail.com>
2025-10-08 16:49:49 -0700

    requests.js edit

--- utils/requests.js
+++ utils/requests.js
@@ -11,7 +11,16 @@ async function fetchProperties({ showFeatured = false } = {}) {
     }
 
     // const res = await fetch(`${apiDomain}/properties${showFeatured ? "/featured" : ""}`, { cache: "force-cache" });
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties${showFeatured ? "/featured" : ""}`, { cache: "force-cache" });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties${showFeatured ? "/featured" : ""}`,
+      { cache: "force-cache" },
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     console.log(res);
 
